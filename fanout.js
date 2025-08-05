22a26afe0967d1e9ab92bcb90ab0aab9dc6aad59
Jin.Park <qkrtjdwls605@naver.com>
2025-08-05 10:03:02 +0900

    Refactor SNS and SQS message handling functions for improved clarity and logging

--- fanout.js
+++ fanout.js
@@ -1,7 +1,6 @@
 const { SNSClient, PublishCommand } = require('@aws-sdk/client-sns');
 const { SQSClient, ReceiveMessageCommand, DeleteMessageCommand } = require('@aws-sdk/client-sqs');
 
-// LocalStack에 연결하기 위한 SNS 클라이언트 설정
 const snsClient = new SNSClient({
   endpoint: 'http://sns.us-east-1.localhost.localstack.cloud:4566', // LocalStack의 Edge 포트
   region: 'ap-northeast-2',
@@ -11,8 +10,7 @@ const snsClient = new SNSClient({
   }
 });
 
-// SNS 메시지 전송 함수
-async function sendSnsMessage(topicArn = '', message = '') {
+async function sendSnsMessage({ topicArn = '', message = '' }) {
   try {
     const command = new PublishCommand({
       TopicArn: topicArn,
@@ -26,10 +24,6 @@ async function sendSnsMessage(topicArn = '', message = '') {
   }
 }
 
-// 실행 예제
-const topicArn = 'arn:aws:sns:ap-northeast-2:000000000000:sample-topic';
-sendSnsMessage(topicArn, 'Hello from LocalStack!');
-
 const sqsClient = new SQSClient({
   region: 'ap-northeast-2',
   endpoint: 'http://sqs.ap-northeast-2.localhost.localstack.cloud:4566',
@@ -39,9 +33,11 @@ const sqsClient = new SQSClient({
   }
 });
 
-// SQS 메시지 수신 함수
 async function receiveSqsMessages(queueUrl = '') {
   try {
+    const queueName = queueUrl.split('/').pop();
+    console.log(`큐 [${queueName}]에서 메시지 수신 시작...`);
+
     const command = new ReceiveMessageCommand({
       QueueUrl: queueUrl,
       MaxNumberOfMessages: 10, // 최대 10개의 메시지를 가져옴
@@ -51,36 +47,41 @@ async function receiveSqsMessages(queueUrl = '') {
     const response = await sqsClient.send(command);
 
     if (response.Messages && response.Messages.length > 0) {
-      console.log('수신된 메시지:', response.Messages);
+      console.log(`큐 [${queueName}]에서 ${response.Messages.length}개의 메시지 수신됨:`, response.Messages);
 
-      // 메시지 삭제
       for (const message of response.Messages) {
         if (message.ReceiptHandle) {
           await deleteSqsMessage(queueUrl, message.ReceiptHandle);
         }
       }
     } else {
-      console.log('메시지가 없습니다.');
+      console.log(`큐 [${queueName}]에 메시지가 없습니다.`);
     }
   } catch (error) {
-    console.error('SQS 메시지 수신 실패:', error);
+    console.error(`큐 [${queueUrl}] 메시지 수신 실패:`, error);
   }
 }
 
-// SQS 메시지 삭제 함수
 async function deleteSqsMessage(queueUrl = '', receiptHandle = '') {
   try {
+    const queueName = queueUrl.split('/').pop();
+
     const command = new DeleteMessageCommand({
       QueueUrl: queueUrl,
       ReceiptHandle: receiptHandle
     });
 
     await sqsClient.send(command);
-    console.log('메시지 삭제 완료:', receiptHandle);
+    console.log(`큐 [${queueName}]에서 메시지 삭제 완료:`, receiptHandle.substring(0, 10) + '...');
   } catch (error) {
-    console.error('메시지 삭제 실패:', error);
+    console.error(`큐 [${queueUrl}]에서 메시지 삭제 실패:`, error);
   }
 }
+
+sendSnsMessage({
+  topicArn: 'arn:aws:sns:ap-northeast-2:000000000000:sample-topic',
+  message: 'Hello, this is a test message!'
+});
 
 receiveSqsMessages('http://sqs.ap-northeast-2.localhost.localstack.cloud:4566/000000000000/sns-subscriber-queue');
 receiveSqsMessages('http://sqs.ap-northeast-2.localhost.localstack.cloud:4566/000000000000/sns-subscriber-queue-2');
